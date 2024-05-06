c41f88c593838415c010f613edb6a8cccb88244c
Mohammad <mohazar9170@gmail.com>
2024-05-06 10:59:25 +0330

    blog detail: primary things

--- blog/urls.py
+++ blog/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.BlogListView.as_view(), name='blog_list'),
+    path('blog/', views.BlogListView.as_view(), name='blog_list'),
     re_path(r'^blog/(?P<slug>[-\w]+)/', views.blog_detail, name='blog_detail'),
-    path('category/homebia/', views.blog_category_homebia, name='category_homebia'),
-    path('category/educational/', views.blog_category_educational, name='category_educational'),
-    path('category/analytical/', views.blog_category_analytical, name='category_analytical'),
-    path('category/news/', views.blog_category_news, name='category_news'),
+    path('blog-category/homebia/', views.blog_category_homebia, name='category_homebia'),
+    path('blog-category/educational/', views.blog_category_educational, name='category_educational'),
+    path('blog-category/analytical/', views.blog_category_analytical, name='category_analytical'),
+    path('blog-category/news/', views.blog_category_news, name='category_news'),
 ]
 
--- config/urls.py
+++ config/urls.py
@@ -19,12 +19,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('pages.urls')),
-    path('blog/', include('blog.urls')),
+    path('', include('blog.urls')),
     path('tinymce/', include('tinymce.urls')),
     path('', include('services.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
