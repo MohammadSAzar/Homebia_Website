b65e817feb68645af2f18aef37b30c3fe5ad2355
Mohammad <mohazar9170@gmail.com>
2024-05-06 12:59:52 +0330

    blogs on homepage

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, redirect
 
+from blog.models import Blog
+
 
 def home_view(request):
-    return render(request, 'pages/home.html')
+    blogs = Blog.objects.select_related('blog_category').filter(status='pub').order_by('-date_creation')[:6]
+    context = {
+        'blogs': blogs
+    }
+    return render(request, 'pages/home.html', context)
 
 
 def services_view(request):
