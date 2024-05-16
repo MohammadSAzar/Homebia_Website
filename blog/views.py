4b289d20a8ee2b8e8fc3b9d04c94de33dfee5483
Mohammad <mohazar9170@gmail.com>
2024-05-16 18:55:59 +0330

    remove of similar Qs

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import FormMixin
 from django.shortcuts import get_object_or_404, render, HttpResponse, reverse
 from django.http import HttpResponseRedirect
+from django.db.models import Prefetch
 from django.utils.translation import gettext as _
 from django.contrib import messages
 
@@ -22,45 +23,42 @@ class BlogDetailView(DetailView):
     def get_queryset(self):
         slug = self.kwargs['slug']
         return Blog.objects.select_related('blog_category').filter(slug=slug)
+        # return Blog.objects.select_related('blog_category').filter(slug=slug)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['blogs'] = Blog.objects.filter(status='pub').values('title', 'date_creation').order_by('-date_creation')[:5]
-
-        context['comments'] = self.object.comments.all()
-        context['replies'] = self.object.blog_replies.all()
-
+        context['comments'] = Comment.objects.prefetch_related('replies__replies').filter(blog=self.object).all()
         context['comment_form'] = CommentForm()
         context['reply_form'] = ReplyForm()
         return context
 
     def post(self, request, *args, **kwargs):
-        if self.request.method == 'POST':
-            if 'reply_name' in self.request.POST:
-                reply_form = ReplyForm(self.request.POST)
-                if reply_form.is_valid():
-                    reply_name = reply_form.cleaned_data['reply_name']
-                    body = reply_form.cleaned_data['body']
+        self.object = self.get_object()
+        if 'reply_name' in self.request.POST:
+            reply_form = ReplyForm(self.request.POST)
+            if reply_form.is_valid():
+                reply_name = reply_form.cleaned_data['reply_name']
+                body = reply_form.cleaned_data['body']
 
-                    if reply_form.cleaned_data['parent_comment'] is not None:
-                        parent_comment = reply_form.cleaned_data['parent_comment']
-                        parent_reply = None
-                    else:
-                        parent_comment = None
-                        parent_reply = reply_form.cleaned_data['parent_reply']
+                if reply_form.cleaned_data['parent_comment'] is not None:
+                    parent_comment = reply_form.cleaned_data['parent_comment']
+                    parent_reply = None
+                else:
+                    parent_comment = None
+                    parent_reply = reply_form.cleaned_data['parent_reply']
 
-                    new_reply = Reply(reply_name=reply_name, body=body, blog=self.get_object(), parent_comment=parent_comment, parent_reply=parent_reply)
-                    new_reply.save()
-            else:
-                comment_form = CommentForm(self.request.POST)
-                if comment_form.is_valid():
-                    name = comment_form.cleaned_data['name']
-                    body = comment_form.cleaned_data['body']
-                    new_comment = Comment(name=name, body=body, blog=self.get_object())
-                    new_comment.save()
+                new_reply = Reply(reply_name=reply_name, body=body, blog=self.object, parent_comment=parent_comment, parent_reply=parent_reply)
+                new_reply.save()
+        else:
+            comment_form = CommentForm(self.request.POST)
+            if comment_form.is_valid():
+                name = comment_form.cleaned_data['name']
+                body = comment_form.cleaned_data['body']
+                new_comment = Comment(name=name, body=body, blog=self.object)
+                new_comment.save()
 
-            # return reverse(self.object.get_absolute_url())
-            return HttpResponseRedirect(reverse('blog_list'))
+        return HttpResponseRedirect(reverse('blog_detail', args=[self.kwargs['slug']]))
 
 
 # class BlogDetailView(FormMixin, DetailView):
@@ -125,4 +123,54 @@ def blog_category_analytical(request):
         'blogs': blogs,
     }
     return render(request, 'blog/blog_category_analytical.html', context)
+
+
+# class BlogDetailView(DetailView):
+#     template_name = 'blog/blog_detail.html'
+#     model = Blog
+#
+#     def get_queryset(self):
+#         slug = self.kwargs['slug']
+#         return Blog.objects.select_related('blog_category').filter(slug=slug)
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['blogs'] = Blog.objects.filter(status='pub').values('title', 'date_creation').order_by('-date_creation')[:5]
+#         context['comments'] = self.object.comments.all()
+#         context['replies'] = self.object.blog_replies.all()
+#         # number_of_comments_and_replies = comments.count() + replies.count()
+#         # context['comments'] = comments
+#         # context['replies'] = replies
+#         # context['number_of_comments_and_replies'] = number_of_comments_and_replies
+#         context['comment_form'] = CommentForm()
+#         context['reply_form'] = ReplyForm()
+#         return context
+#
+#     def post(self, request, *args, **kwargs):
+#         if self.request.method == 'POST':
+#             if 'reply_name' in self.request.POST:
+#                 reply_form = ReplyForm(self.request.POST)
+#                 if reply_form.is_valid():
+#                     reply_name = reply_form.cleaned_data['reply_name']
+#                     body = reply_form.cleaned_data['body']
+#
+#                     if reply_form.cleaned_data['parent_comment'] is not None:
+#                         parent_comment = reply_form.cleaned_data['parent_comment']
+#                         parent_reply = None
+#                     else:
+#                         parent_comment = None
+#                         parent_reply = reply_form.cleaned_data['parent_reply']
+#
+#                     new_reply = Reply(reply_name=reply_name, body=body, post=self.get_object(), parent_comment=parent_comment, parent_reply=parent_reply)
+#                     new_reply.save()
+#             else:
+#                 comment_form = CommentForm(self.request.POST)
+#                 if comment_form.is_valid():
+#                     name = comment_form.cleaned_data['name']
+#                     body = comment_form.cleaned_data['body']
+#                     new_comment = Comment(name=name, body=body, post=self.get_object())
+#                     new_comment.save()
+#
+#             # return reverse(self.object.get_absolute_url())
+#             return HttpResponseRedirect(reverse('blog_list'))
 
