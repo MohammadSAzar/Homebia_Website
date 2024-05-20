bf5a536525b9423373f2dbd160e7211d37eabbdd
Mohammad <mohazar9170@gmail.com>
2024-05-20 13:05:53 +0330

    finalizing blog comment Qs + add text area to form body

--- blog/forms.py
+++ blog/forms.py
@@ -7,11 +7,11 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['name', 'body']
-        widgets = {'body': forms.TextInput()}
+        widgets = {'body': forms.Textarea()}
 
 class ReplyForm(forms.ModelForm):
     class Meta:
         model = Reply
         fields = ['reply_name', 'body', 'parent_comment', 'parent_reply']
-        widgets = {'body': forms.TextInput()}
+        widgets = {'body': forms.Textarea()}
 
