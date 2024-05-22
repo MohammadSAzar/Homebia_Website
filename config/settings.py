8e22a0630e86f35f491da4142a11d5a8edd3f6e3
Mohammad <mohazar9170@gmail.com>
2024-05-22 16:23:06 +0330

    pages translations

--- config/settings.py
+++ config/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'jalali',
     'pages',
     'services',
+    'templates',  # for apply translations!
 ]
 
 MIDDLEWARE = [
@@ -121,11 +122,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Language $ Translation
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa'
+# LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'Asia/Tehran'
+# TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L18N = True
-USE_TZ = False
+USE_TZ = True
 
 
 # Default primary key field type
