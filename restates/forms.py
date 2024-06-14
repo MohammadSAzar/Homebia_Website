76b6d688ec2420c88955b77a1b7777050e64f9b3
Mohammad <mohazar9170@gmail.com>
2024-06-14 15:09:09 +0330

    file creat form template

--- restates/forms.py
+++ restates/forms.py
@@ -1,11 +1,13 @@
 from django import forms
-from .models import SaleFile, generate_unique_id
 from django.utils import timezone
 from django.utils.text import slugify
 
+from .models import SaleFile, generate_unique_id
+from . import checkers
+
 
 create_file_fields = ['province', 'city', 'district', 'price', 'room', 'area', 'year', 'document', 'level', 'parking', 'elevator',
-                      'warehouse', 'document', 'title', 'description', 'cover', 'cover2', 'cover3', 'cover4',
+                      'warehouse', 'title', 'description', 'cover', 'cover2', 'cover3', 'cover4',
                       'direction', 'file_levels', 'aparts_per_level', 'balcony', 'bench_stove', 'restoration', 'toilet',
                       'hot_water', 'cooling', 'heating', 'floor', 'provider_name', 'phone_number_for_contact',
                       'provider_national_code', 'owner_national_code', 'file_postal_code']
@@ -19,5 +21,34 @@ class SaleFileCreateForm(forms.ModelForm):
             'city': forms.Select(attrs={'id': 'city'}),
             'district': forms.Select(attrs={'id': 'district'})
         }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        price = cleaned_data.get('price')
+        area = cleaned_data.get('area')
+        phone_number_for_contact = cleaned_data.get('phone_number_for_contact')
+        file_postal_code = cleaned_data.get('file_postal_code')
+        provider_national_code = cleaned_data.get('provider_national_code')
+        owner_national_code = cleaned_data.get('owner_national_code')
+
+        if price and not checkers.file_price_checker(price):
+            self.add_error('price', 'قیمت فایل باید بین 1 تا 1000 میلیارد تومان باشد')
+
+        if area and not checkers.area_checker(area):
+            self.add_error('area', 'متراژ فایل باید بین 1 تا 10000 متر باشد.')
+
+        if phone_number_for_contact and not checkers.phone_checker(phone_number_for_contact):
+            self.add_error('phone_number_for_contact', 'شماره تلفن همراه وارد شده معتبر نیست.')
+
+        if file_postal_code and not checkers.postal_code_checker(file_postal_code):
+            self.add_error('file_postal_code', 'کد پستی وارد شده معتبر نیست.')
+
+        if provider_national_code and not checkers.national_code_checker(provider_national_code):
+            self.add_error('provider_national_code', 'کد ملی وارد شده معتبر نیست.')
+
+        if owner_national_code and not checkers.national_code_checker(owner_national_code):
+            self.add_error('owner_national_code', 'کد ملی وارد شده معتبر نیست.')
+
+        return cleaned_data
 
 
--- restates/views.py
+++ restates/views.py
@@ -6,16 +6,6 @@ from django.contrib import messages
 
 from .models import SaleFile, City, District
 from .forms import SaleFileCreateForm
-from . import checkers
-
-
-checks_list = [('price', checkers.file_price_checker, 'قیمت فایل باید بین 1 تا 1000 میلیارد تومان باشد'),
-               ('area', checkers.area_checker, 'متراژ فایل باید بین 1 تا 10000 متر باشد.'),
-               ('phone_number_for_contact', checkers.phone_checker, 'شماره تلفن همراه وارد شده معتبر نیست.'),
-               ('file_postal_code', checkers.postal_code_checker, 'کد پستی وارد شده معتبر نیست.'),
-               ('provider_national_code', checkers.national_code_checker, 'کد ملی وارد شده معتبر نیست.'),
-               ('owner_national_code', checkers.national_code_checker, 'کد ملی وارد شده معتبر نیست.')
-               ]
 
 
 class SaleFileCreateView(CreateView):
@@ -32,38 +22,53 @@ class SaleFileCreateView(CreateView):
             return self.form_invalid(form)
 
     def form_valid(self, form):
-        checks = []
-        if not checkers.file_price_checker(form.cleaned_data['price']):
-            checks.append(False)
-        else:
-            checks.append(True)
-        if not checkers.area_checker(form.cleaned_data['area']):
-            checks.append(False)
-        else:
-            checks.append(True)
-        if not checkers.phone_checker(form.cleaned_data['phone_number_for_contact']):
-            checks.append(False)
-        else:
-            checks.append(True)
-        if not checkers.postal_code_checker(form.cleaned_data['file_postal_code']):
-            checks.append(False)
-        else:
-            checks.append(True)
-        if not checkers.national_code_checker(form.cleaned_data['provider_national_code']):
-            checks.append(False)
-        else:
-            checks.append(True)
-        if not checkers.national_code_checker(form.cleaned_data['owner_national_code']):
-            checks.append(False)
-        else:
-            checks.append(True)
-        if False not in checks:
-            return super().form_valid(form)
-        else:
-            return super().form_invalid(form)
+        messages.success(self.request, "آگهی شما پس از تایید ادمین در سایت منتشر خواهد شد.")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        self.object = None
+        return self.render_to_response(self.get_context_data(form=form))
 
     def get_success_url(self):
-        return reverse('home')
+        return reverse('file_list')
+
+
+def load_cities(request):
+    province_id = request.GET.get('province')
+    cities = City.objects.filter(province_id=province_id).order_by('name')
+    city_choices = [(city.id, city.name) for city in cities]
+    return JsonResponse({'cities': city_choices})
+
+
+def load_districts(request):
+    city_id = request.GET.get('city')
+    districts = District.objects.filter(city_id=city_id).order_by('name')
+    district_choices = [(district.id, district.name) for district in districts]
+    return JsonResponse({'districts': district_choices})
+
+
+class SaleFileDetailView(DetailView):
+    model = SaleFile
+    context_object_name = 'sale_file'
+    template_name = 'files/file_detail.html'
+
+
+class SaleFileListView(ListView):
+    model = SaleFile
+    paginate_by = 6
+    context_object_name = 'files'
+    template_name = 'files/file_list.html'
+
+
+# def sale_file_create_view(request):
+#     if request.method == 'POST':
+#         form = SaleFileCreateForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('home')
+#     else:
+#         form = SaleFileCreateForm()
+#     return render(request, 'files/file_create.html', {'form': form})
 
 
 # def sale_file_create_view(request):
@@ -88,38 +93,4 @@ class SaleFileCreateView(CreateView):
 #         form = SaleFileCreateForm()
 #     return render(request, 'files/file_create.html', {'form': form})
 
-
-def load_cities(request):
-    province_id = request.GET.get('province')
-    cities = City.objects.filter(province_id=province_id).order_by('name')
-    city_choices = [(city.id, city.name) for city in cities]
-    return JsonResponse({'cities': city_choices})
-
-def load_districts(request):
-    city_id = request.GET.get('city')
-    districts = District.objects.filter(city_id=city_id).order_by('name')
-    district_choices = [(district.id, district.name) for district in districts]
-    return JsonResponse({'districts': district_choices})
-
-class SaleFileDetailView(DetailView):
-    model = SaleFile
-    context_object_name = 'sale_file'
-    template_name = 'files/file_detail.html'
-
-class SaleFileListView(ListView):
-    model = SaleFile
-    paginate_by = 6
-    context_object_name = 'files'
-    template_name = 'files/file_list.html'
-
-
-# def sale_file_create_view(request):
-#     if request.method == 'POST':
-#         form = SaleFileCreateForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('home')
-#     else:
-#         form = SaleFileCreateForm()
-#     return render(request, 'files/file_create.html', {'form': form})
 
