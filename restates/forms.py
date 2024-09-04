e39970e9c6b1b08d8039cf16c672312b59af2372
Mohammad <mohazar9170@gmail.com>
2024-09-04 13:28:42 +0330

    files creation forms validations

--- restates/forms.py
+++ restates/forms.py
@@ -20,6 +20,16 @@ create_rent_file_fields = ['province', 'city', 'district', 'price_deposit', 'pri
                            'provider_national_code', 'owner_national_code', 'file_postal_code']
 
 
+sale_required_fields = ['province', 'city', 'price', 'room', 'area', 'age', 'document', 'level', 'parking', 'elevator',
+                        'warehouse', 'title', 'description', 'cover', 'provider_name', 'phone_number_for_contact',
+                        'provider_national_code', 'owner_national_code', 'file_postal_code']
+
+
+rent_required_fields = ['province', 'city', 'price_deposit', 'price_rent', 'convertable', 'room', 'area', 'age', 'document',
+                        'level', 'parking', 'elevator', 'warehouse', 'title', 'description', 'cover', 'provider_name',
+                        'phone_number_for_contact', 'provider_national_code', 'owner_national_code', 'file_postal_code']
+
+
 class SaleFileCreateForm(forms.ModelForm):
     class Meta:
         model = SaleFile
@@ -29,6 +39,11 @@ class SaleFileCreateForm(forms.ModelForm):
             'city': forms.Select(attrs={'id': 'city'}),
             'district': forms.Select(attrs={'id': 'district'})
         }
+
+    def __init__(self, *args, **kwargs):
+        super(SaleFileCreateForm, self).__init__(*args, **kwargs)
+        for field in sale_required_fields:
+            self.fields[field].required = True
 
     def clean(self):
         cleaned_data = super().clean()
@@ -41,19 +56,14 @@ class SaleFileCreateForm(forms.ModelForm):
 
         if price and not checkers.file_price_checker(price):
             self.add_error('price', 'قیمت فایل باید بین 1 تا 1000 میلیارد تومان باشد')
-
         if area and not checkers.area_checker(area):
             self.add_error('area', 'متراژ فایل باید بین 20 تا 10000 متر باشد.')
-
         if phone_number_for_contact and not checkers.phone_checker(phone_number_for_contact):
             self.add_error('phone_number_for_contact', 'شماره تلفن همراه وارد شده معتبر نیست.')
-
         if file_postal_code and not checkers.postal_code_checker(file_postal_code):
             self.add_error('file_postal_code', 'کد پستی وارد شده معتبر نیست.')
-
         if provider_national_code and not checkers.national_code_checker(provider_national_code):
             self.add_error('provider_national_code', 'کد ملی وارد شده معتبر نیست.')
-
         if owner_national_code and not checkers.national_code_checker(owner_national_code):
             self.add_error('owner_national_code', 'کد ملی وارد شده معتبر نیست.')
 
@@ -111,6 +121,11 @@ class RentFileCreateForm(forms.ModelForm):
             'district': forms.Select(attrs={'id': 'district'})
         }
 
+    def __init__(self, *args, **kwargs):
+        super(RentFileCreateForm, self).__init__(*args, **kwargs)
+        for field in rent_required_fields:
+            self.fields[field].required = True
+
     def clean(self):
         cleaned_data = super().clean()
         price_deposit = cleaned_data.get('price_deposit')
@@ -120,6 +135,7 @@ class RentFileCreateForm(forms.ModelForm):
         file_postal_code = cleaned_data.get('file_postal_code')
         provider_national_code = cleaned_data.get('provider_national_code')
         owner_national_code = cleaned_data.get('owner_national_code')
+        description = cleaned_data.get('description')
 
         if price_deposit and not checkers.rent_file_deposit_price_checker(price_deposit):
             self.add_error('price_deposit', 'مبلغ ودیعه باید بین 0 تا 100 میلیارد تومان باشد')
@@ -141,6 +157,9 @@ class RentFileCreateForm(forms.ModelForm):
 
         if owner_national_code and not checkers.national_code_checker(owner_national_code):
             self.add_error('owner_national_code', 'کد ملی وارد شده معتبر نیست.')
+
+        if not description:
+            self.add_error('description', 'این فیلد لازم است.')
 
         return cleaned_data
 
