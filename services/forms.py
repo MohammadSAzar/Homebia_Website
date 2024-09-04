9731ff50ec205fd76a380afe1acd7950ac00d39d
Mohammad <mohazar9170@gmail.com>
2024-09-04 11:12:51 +0330

    services forms validations

--- services/forms.py
+++ services/forms.py
@@ -7,16 +7,31 @@ class CounselingForm(forms.ModelForm):
         model = models.Counseling
         fields = ['counseling_type', 'date', 'time', 'name_and_family']
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.required = True
+
 
 class SessionForm(forms.ModelForm):
     class Meta:
         model = models.Session
         fields = ['customer_type', 'city', 'date', 'time', 'name_and_family']
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.required = True
+
 
 class VisitForm(forms.ModelForm):
     class Meta:
         model = models.Visit
         fields = ['city', 'date', 'time', 'name_and_family']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.required = True
 
 
